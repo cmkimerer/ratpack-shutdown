7f98605e92951a28c47616aaf0f7bfc030812fc2
Craig Kimerer <craig@juvo.com>
2016-02-03 16:40:52 -0800

    Example of a ratpack app that fails to shutdown services

---
+++ src/main/java/com/example/shutdown/DumbService.java
@@ -0,0 +1,13 @@
+package com.example.shutdown;
+
+import ratpack.server.Service;
+import ratpack.server.StartEvent;
+import ratpack.server.StopEvent;
+
+public class DumbService implements Service {
+  public void onStart(StartEvent startEvent) {
+  }
+
+  public void onStop(StopEvent stopEvent) {
+  }
+}
---
+++ src/main/java/com/example/shutdown/Shutdown.java
@@ -0,0 +1,50 @@
+
+package com.example.shutdown;
+
+import ratpack.server.RatpackServer;
+import ratpack.server.ServerConfig;
+
+public class Shutdown {
+
+  public static void main(String... args) throws Exception {
+
+    Wrapper wrapper = new Wrapper();
+
+    RatpackServer server = RatpackServer.of(s ->
+        s.serverConfig(
+            // Note: Shutdown fails when threads is <= 10, sometimes fails when it's just a little
+            // higher than 10
+            ServerConfig.builder()
+            .threads(4)
+        ).registryOf(registry -> {
+          for (int i = 0; i < 10; i++) {
+            registry.add(new DumbService());
+          }
+        }).handlers(chain ->
+            chain.all(ctx -> {
+              wrapper.stop();
+              ctx.render("");
+            })
+        )
+    );
+
+    wrapper.setServer(server);
+
+    server.start();
+  }
+
+  private static class Wrapper {
+    RatpackServer server;
+    public Wrapper() {
+    }
+
+    public void setServer(RatpackServer server) {
+      this.server = server;
+    }
+
+    public void stop() throws Exception {
+      server.stop();
+    }
+
+  }
+}
