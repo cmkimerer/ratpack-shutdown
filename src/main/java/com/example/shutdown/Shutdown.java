249a459e42caea615c8e9a81df30fcc44737729c
Craig Kimerer <craig@juvo.com>
2016-04-27 19:29:31 -0700

    Update to the latest version of ratpack & add blocking & non blocking handler

--- src/main/java/com/example/shutdown/Shutdown.java
+++ src/main/java/com/example/shutdown/Shutdown.java
@@ -1,6 +1,7 @@
 
 package com.example.shutdown;
 
+import ratpack.exec.Blocking;
 import ratpack.server.RatpackServer;
 import ratpack.server.ServerConfig;
 
@@ -15,19 +16,31 @@ public class Shutdown {
             // Note: Shutdown fails when threads is <= 10, sometimes fails when it's just a little
             // higher than 10
             ServerConfig.builder()
-            .threads(4)
-        ).registryOf(registry -> {
-          for (int i = 0; i < 10; i++) {
-            registry.add(new DumbService());
-          }
-        }).handlers(chain ->
-            chain.all(ctx -> {
-              System.out.println("Shutdown is being called");
-              wrapper.stop();
-              ctx.render("");
-            })
-        )
-    );
+                .development(false)
+                .threads(4)
+        ).handlers(chain ->
+            chain.prefix("shutdown", shutdown -> shutdown
+                .all(ctx -> {
+                  System.out.println("Shutdown is being called");
+                  ctx.onClose(outcome -> new Thread(wrapper::stop).start());
+                  ctx.render("");
+                })).prefix("sleep_blocking", sleep -> sleep
+                .all(ctx -> {
+                  // sleep 10s
+                  Blocking.get(() -> {
+                    System.out.println("Request started, sleeping 10s");
+                    Thread.sleep(10000);
+                    System.out.println("Request finished, returning result now");
+                    return true;
+                  }).then(x -> ctx.getResponse().send("This request has finished"));
+                })).prefix("sleep", sleep -> sleep
+                .all(ctx -> {
+                  // sleep 10s
+                  System.out.println("Request started, sleeping 10s");
+                  Thread.sleep(10000);
+                  System.out.println("Request finished, returning result now");
+                  ctx.getResponse().send("This request has finished");
+                }))));
 
     wrapper.setServer(server);
 
@@ -43,9 +56,13 @@ public class Shutdown {
       this.server = server;
     }
 
-    public void stop() throws Exception {
-      server.stop();
+    public void stop() {
+      try {
+        System.out.println("Shutdown called");
+        server.stop();
+      } catch (Exception ex) {
+        System.out.println("Failed to shutdown with error " + ex.getMessage());
+      }
     }
-
   }
 }
